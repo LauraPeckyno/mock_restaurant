0728fa5eb028a1a0aefc59d91d304d000540dc55
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-08-19 11:57:34 -0400

    adding an images file to potentialy handle those assets

---
+++ images.mjs
@@ -0,0 +1,20 @@
+// https://images.unsplash.com/photo-1511920170033-f8396924c348?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+// coffee
+
+// https://images.unsplash.com/photo-1483648969698-5e7dcaa3444f?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+// tables
+
+// https://plus.unsplash.com/premium_photo-1673522561650-0e9b68be2ca3?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+// outdoor seating
+
+// https://images.unsplash.com/photo-1604882406305-67a5b1376126?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+// baked goods
+
+// https://plus.unsplash.com/premium_photo-1674930412380-1c394135677d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+// barista
+
+// https://plus.unsplash.com/premium_photo-1673830186289-b4d2619212b3?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+// catering
+
+// https://plus.unsplash.com/premium_photo-1661479328777-e1692d224ae6?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+// catering 2
